ff39664cbc007b0b89882e23dbeead8f2a2da07a
Mushegh Andrasyan <mushegh.andrasyan@gmail.com>
2022-08-27 18:15:38 +0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const API_URL = `https://jsonplaceholder.typicode.com/posts`
+
+const postsNode = document.getElementById('posts')
+const postsGetButtonNode = document.getElementById('postsGetButton')
+
+const getPosts = () => {
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(el => {
+                const newLiNode = document.createElement('li')
+                const newTitleNode = document.createElement('div')
+                const newBodyNode = document.createElement('div')
+
+                newTitleNode.innerHTML = el.title
+                newBodyNode.innerHTML = el.body
+                newLiNode.appendChild(newTitleNode)
+                newLiNode.appendChild(newBodyNode)
+
+                postsNode.appendChild(newLiNode)
+            });
+        })
+}
+
+postsGetButtonNode.addEventListener('click', getPosts)

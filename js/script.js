592c7795caece833fa1201fbc0ef917cb2d8b9a1
Mushegh Andrasyan <mushegh.andrasyan@gmail.com>
2022-09-03 13:58:54 +0400

    adds filter logic

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@ const API_URL = `https://jsonplaceholder.typicode.com/posts`
 
 const postsNode = document.getElementById('posts')
 const postsGetButtonNode = document.getElementById('postsGetButton')
+const filterInputNode = document.getElementById('filter-input')
+
+let store;
 
 const getPosts = () => {
     fetch(API_URL)
         .then(response => response.json())
         .then(data => {
+            store = data
             data.forEach(el => {
+                
                 const newLiNode = document.createElement('li')
                 const newTitleNode = document.createElement('div')
                 const newBodyNode = document.createElement('div')
@@ -22,4 +27,15 @@ const getPosts = () => {
         })
 }
 
+function filterPosts(userId, posts){
+    console.log(posts)
+    return posts.filter(post => post.userId === userId)
+}
+
+filterInputNode.addEventListener('change', () => {
+    const value = filterInputNode.value
+    console.log(value)
+    const filteredPosts = filterPosts(+value, store)
+    console.log(filteredPosts)
+})
 postsGetButtonNode.addEventListener('click', getPosts)

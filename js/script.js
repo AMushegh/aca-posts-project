31104661719647a19f0276c7860b2dd7cdf1760a
Mushegh Andrasyan <mushegh.andrasyan@gmail.com>
2022-11-26 17:14:15 +0400

    deletes solution

--- js/script.js
+++ js/script.js
@@ -1,25 +1,4 @@
-const API_URL = `https://jsonplaceholder.typicode.com/posts`
+const API_URL = `https://jsonplaceholder.typicode.com/posts`;
 
-const postsNode = document.getElementById('posts')
-const postsGetButtonNode = document.getElementById('postsGetButton')
-
-const getPosts = () => {
-    fetch(API_URL)
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(el => {
-                const newLiNode = document.createElement('li')
-                const newTitleNode = document.createElement('div')
-                const newBodyNode = document.createElement('div')
-
-                newTitleNode.innerHTML = el.title
-                newBodyNode.innerHTML = el.body
-                newLiNode.appendChild(newTitleNode)
-                newLiNode.appendChild(newBodyNode)
-
-                postsNode.appendChild(newLiNode)
-            });
-        })
-}
-
-postsGetButtonNode.addEventListener('click', getPosts)
+const postsNode = document.getElementById("posts");
+const postsGetButtonNode = document.getElementById("postsGetButton");
